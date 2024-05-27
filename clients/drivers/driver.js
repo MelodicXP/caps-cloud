685af11462a6b6f656cb539effeada7fb76b6158
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-05-27 18:58:41 -0500

    Ability to send message to vendor sqs queue of delivery confirmation

--- clients/drivers/driver.js
+++ clients/drivers/driver.js
@@ -1,24 +1,24 @@
 'use strict';
 
 // Import modules
-const { SQSClient, ReceiveMessageCommand, DeleteMessageCommand } = require('@aws-sdk/client-sqs');
+const { SQSClient, ReceiveMessageCommand, DeleteMessageCommand, SendMessageCommand } = require('@aws-sdk/client-sqs');
 
 //Configure SQS client
 const sqsClient = new SQSClient({ region: 'us-east-1'});
 
-//Define SQS url
+// Define SQS url
 const QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/533267271730/packages.fifo';
 
-// Define SQS parameters to receive a message
-const sqsParams = {
-  QueueUrl: QUEUE_URL,
-  MaxNumberOfMessages: 1,
-  WaitTimeSeconds: 10,
-};
-
-// Function to receive and delete messages from the SQS queue
+// Function to receive, process and send messages from the SQS queue
 const processMessages = async () => {
   try {
+    // Define SQS parameters to receive a message
+    const sqsParams = {
+      QueueUrl: QUEUE_URL,
+      MaxNumberOfMessages: 1,
+      WaitTimeSeconds: 10,
+    };
+
     // Receive a message from the SQS queue
     const receiveCommand = new ReceiveMessageCommand(sqsParams);
     const receiveResponse = await sqsClient.send(receiveCommand);
@@ -29,6 +29,31 @@ const processMessages = async () => {
 
       // Log the received message
       console.log('Package received:', message.Body);
+
+      // Parse the JSON message body
+      const parsedMessageBody = JSON.parse(message.Body);
+      const parsedMessage = JSON.parse(parsedMessageBody.Message);
+      console.log('parsedMessage:', parsedMessage);
+      const orderId = parsedMessage.orderId;
+      const url = parsedMessage.url;
+
+      // Log the extracted details, url used to send to vendor SQS queue
+      console.log(`Order ID: ${orderId}`);
+      console.log(`URL: ${url}`);
+
+      // Build the new message to send to vendor SQS queue
+      const deliveredMessage = `Package for order id ${orderId}, has been delivered`;
+
+      // Parameters for sending a new message
+      const sendParams = {
+        QueueUrl: url, // Use the extracted URL
+        MessageBody: deliveredMessage,
+      };
+
+       // Send delivered message to the vendor SQS queue
+       const sendCommand = new SendMessageCommand(sendParams);
+       const sendResponse = await sqsClient.send(sendCommand);
+       console.log('Message sent:', sendResponse);
 
       // Parameters for deleting the message
       const deleteParams = {
